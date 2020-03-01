ae6627536ece151fa98782592095ed71034b326c
yimen3 <47582621+yimen3@users.noreply.github.com>
2020-03-01 12:39:37 -0500

    Update machine learning.py

--- machine learning.py
+++ machine learning.py
@@ -1,6 +1,6 @@
-from sklearn import tree
-features = [[140,1],[175,2],[175,1],[140,2]]
-labels = [0,0,1,1]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features,labels)
-print(clf.predict[140,2])
+from sklearn import tree #import the library
+features = [[140,1],[175,2],[175,1],[140,2]] #input
+labels = [0,0,1,1] #output
+clf = tree.DecisionTreeClassifier() #make a classifier
+clf = clf.fit(features,labels) #classify
+print(clf.predict[140,2]) #predict
